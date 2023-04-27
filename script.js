e1a785006e5ddfbf3c2731219d6fc7a60bf79870
IbragimovTimur14 <129717285+IbragimovTimur14@users.noreply.github.com>
2023-04-27 20:10:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,377 @@
+"use strict";
+
+
+// Masalalar
+
+// ESLATMA
+// har bir masalani alohida alohida commentdan chiqaring, hammasini birdan chiqarmang !!!
+
+
+
+
+//1
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log(3);
+// } else if ((a > 0 && b > 0) || (b >0 && c > 0) || (a > 0 && c > 0)) {
+//     console.log(2);
+// } else if (a > 0 || b > 0 || c > 0) {
+//     console.log(1);
+// } else {
+//     console.log(0);
+// }
+
+
+// 2
+
+// let a = +prompt("a= ");
+// let b = (a - (a % 3)) / 3;
+// console.log(b);
+
+//3
+
+// let a = +prompt("a= ");
+// console.log(a % 3);
+
+//4
+
+// let a = +prompt("son kiriting: ");
+// if (a % 2 == 0) {
+//   console.log("Juft");  
+// } else {
+//     console.log("Toq");
+// }
+
+//5
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+
+// if (a > b) {
+//     console.log(a);
+// } else if (b > a) {
+//     console.log(b);
+// } else {
+//     console.log("Voy ular teng.");
+// }
+
+//6
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if (a > b && a > c) {
+//     console.log(a + " soni katta");
+// } else if (b >a && b > c) {
+//     console.log(b + " soni katta");  
+// } else if ( c > a && c > b) {
+//     console.log(c + " soni katta");  
+// } else {
+//     console.log("Ular teng");
+// }
+
+// 7
+
+// let a = +prompt("a= ");
+// if (a > 0) {
+//   console.log("musbat");  
+// } else if (a < 0) {
+//     console.log("manfiy");
+// } else {
+//     console.log(0);
+// }
+
+//8
+
+//  let a = +prompt("a= ");
+//  if (a % 5 == 0) {
+//     console.log(true);
+//  } else {
+//     console.log(false);
+//  }
+
+
+// 9
+
+// let a = +prompt("a= ");
+// if (a % 3 ==0 && a % 4 ==0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 10
+
+// let yil = +prompt("yil= ");
+// if (yil % 4 ==0 && yil % 100 != 0 || yil % 400 == 0) {
+//     console.log("Kabisa");
+// } else {
+//     console.log("Kabisa emas");
+// }
+
+
+// 11
+
+// let char = prompt("char: ");
+// if (char >= "a" && char <= "z") {
+//     console.log(char, "alpha");
+// } else if (char >= 0 && char <= 9) {
+//     console.log(char, "son");
+// } else {
+//     console.log(char, "boshqa narsa");
+// }
+
+// 12
+
+// let c = prompt("char: ");
+// if (c >= "a" && c <= "z") {
+//     console.log("lowercase");
+// } else if (c >= "A" && c <= "Z") {
+//     console.log("uppercase");
+// } else {
+//     console.log("none");
+// }
+
+
+// 13
+
+// let a = +prompt("Son kiriting:");
+// if (a == 1) {
+//  console.log("Dushanba");    
+// } else if (a == 2) {
+//  console.log("Seshanba");     
+// } else if (a == 3) {
+//     console.log("Chorshanba");
+// } else if (a == 4) {
+//     console.log("Payshanba");
+// } else if (a == 5) {
+//     console.log("Juma");
+// } else if (a == 6) {
+//     console.log("Shanba");
+// } else if (a == 7) {
+//     console.log("Yakshanba");
+// } else {
+//     console.log("none");
+// }
+
+
+// 14 
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if (a + b + c == 180) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 15
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if ((a + b) > c && (a + c) > b && (b + c) > a) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 16
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if (a == b && b == c && a == c) {
+//     console.log("equilateral");
+// } else if (a == b) {
+//     console.log("isosceles");
+// } else if (b == c) {
+//     console.log("isosceles");
+// } else if (a == c) {
+//     console.log("isosceles");
+// } 
+// else {
+//     console.log("scalene");
+// }
+
+
+// 17 
+
+// 1 bilan bir xil
+
+// 18
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// if (a < b) {
+//     console.log(a + " soni kichik");
+// } else if (b < a) {
+//     console.log(b + " soni kichik");
+// } else {
+//     console.log("teng");
+// }
+
+// 19
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// console.log((a + b + c) / 3);
+
+
+// 20
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+//  if (a < b && b < c) {
+//     console.log(1);
+//  } else if (a > b && b > c) {
+//     console.log(2);
+//  } else if (b > a && b > c) {
+//     console.log(b);
+//  } else if (a == b || b == c || a == c) {
+//     console.log(5);
+//  }  else {
+//     console.log(0);
+//  }
+
+// 21
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if (a == b) {
+//     console.log(c);
+
+// } else if (b == c) {
+//     console.log(a);
+// } else if (a == c) {
+//     console.log(b);
+// } else {
+//     console.log(0);
+// }
+
+// 22
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// if (a > 0 && b > 0 && a > b) {
+//     console.log(a - b);
+// } else if (a > 0 && b > 0 && a < b) {
+//     console.log(b - a);
+// }  else if (a < 0 && b < 0 && a > b) {
+//     console.log(--a - --b);
+// }  else if (a < 0 && b < 0 && a < b) {
+//     console.log(--b - --a);
+// } else if (a < 0 && b > 0) {
+//     console.log(b - a);
+// } else if (a > 0 && b < 0) {
+//     console.log(a - b);
+// } else if (a == 0 && b== 0) {
+//     console.log(0);
+// } else if (a > 0 && b ==0) {
+//     console.log(a);
+// } else if (a < 0 && b ==0) {
+//     console.log(-a);
+// } else if (a == 0 && b > 0) {
+//     console.log(b);
+// } else if (a == 0 && b < 0) {
+//     console.log(-b);
+// } else {
+//     console.log("none");
+// }
+
+
+// 23
+
+// let a = +prompt("a= ");
+// if (a >= 0) {
+//     console.log(a);
+// } else {
+//     console.log(-a);
+// }
+
+
+// 24
+
+// let a = +prompt("a= ");
+
+// if (a >= 1 && a < 10) {
+//     console.log(1);
+// } else if (a >= 10 && a < 100) {
+//     console.log(2);
+// } else if (a >=100 && a <= 999) {
+//     console.log(3);
+// } else {
+//     console.log("none");
+// }
+
+// 25
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+// let d = +prompt("d= ");
+
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+// 26
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if (a % 2 == 1 && b % 2 == 1 && c % 2 == 1) {
+//     console.log(1);
+// } else if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+//     console.log(2);
+// } else if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) {
+//     console.log(3);
+// } else {
+//     console.log(0);
+// }
+
+// 27
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if ((a % 2 == 0 && b % 2 ==0) || (c % 2 == 0 && b % 2 ==0) || (a % 2 == 0 && c % 2 ==0)) {
+//     console.log(1);
+// } else if ((a % 2 == 1 && b % 2 == 1) || (c % 2 == 1 && b % 2 == 1) || (a % 2 == 1 && c % 2 == 1)) {
+//     console.log(2);
+// } else {
+//     console.log(0);
+// }
+
+// 28
+
+// let a = +prompt("a= ");
+// let b = +prompt("b= ");
+// let c = +prompt("c= ");
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log(a * 100 + b * 10 + c);
+// } else {
+//     console.log(0);
+// }
+
+
+// 10 11 12 15 
